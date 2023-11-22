96eee073a805e2b79f7f8d3996886b9f8ece4058
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-11-22 09:41:09 +0100

    created a post component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Post from './Post';
 
 function App ()  {
   return (
@@ -8,6 +9,7 @@ function App ()  {
       < Navbar />
       <h1>Welcome to My Blog!</h1>
       {/* Add your blog posts here */}
+      < Post username="Gosia" message= "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups." timestamp="09:36"/>
       < Footer />
     </div>
   );
